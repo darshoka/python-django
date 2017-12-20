c800930e3ffc9533d76f6ac4812ddf4f3e7af3c2
Adarsh Kadameri <adarshkadameri7@gmail.com>
2017-12-21 01:06:29 +0530

    Enhancement

--- music/views.py
+++ music/views.py
@@ -1,20 +1,18 @@
 from django.http import Http404
 from .models import Album
-from django.shortcuts import render
-#from django.template import loader
+from django.shortcuts import render, get_object_or_404
+# from django.template import loader
 
 
 def index(request):
     all_albums = Album.objects.all()
-    #template = loader.get_template('music/index.html')
+    # template = loader.get_template('music/index.html')
     return render(request, 'music/index.html', {'all_albums': all_albums})
-    #return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
-    #return HttpResponse("<h2> Details for  Album id :  " + str(album_id) + "</h2>")
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    # return HttpResponse("<h2> Details for  Album id :  " + str(album_id) + "</h2>")
+    # album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
